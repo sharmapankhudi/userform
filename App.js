3415bfc8c74fca88e10997fb75b520beb059f005
sharmapankhudi <er.pankhudi07@gmail.com>
2020-07-30 13:37:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
 import Registration from './Registration'
- 
+ import UserManagement from './UserManagement';
 function App() {
   const [name, setName] = useState([])
   
   return (
     <>
+      <UserManagement />
       <Registration title="Form Registartion" 
       name={name} setName={setName} 
 
