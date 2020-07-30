3c55e8fc5bdf849ded994c259df875c4b829ccb3
sharmapankhudi <er.pankhudi07@gmail.com>
2020-07-30 13:36:36 +0530

    Add files via upload

---
+++ UserManagement.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UserManagement = () => {
+    return(
+        <div className="header">
+            <p>UserManagement</p>
+        </div> 
+    )
+}
+export default UserManagement;
